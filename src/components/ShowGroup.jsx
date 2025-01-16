a268cf492027a9038dc85b3c189f9b282564bbcf
Sachin Raj <sachinraj6392@gmail.com>
2025-01-16 14:11:48 +0530

    add api , card ,his css

--- src/components/ShowGroup.jsx
+++ src/components/ShowGroup.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import style from './Showgroup.module.css';
 import GroupAddForm from "./GroupForm";
 
@@ -6,9 +6,46 @@ import GroupAddForm from "./GroupForm";
 function ShowGroup() {
 
     const [formShow, setFormShow] = useState(false);
+    const [data, setData] = useState([]);
+    const [error, setError] = useState(null);
 
     function FormShowHide() {
         setFormShow(!formShow);
+    }
+
+    useEffect(() => {
+        async function fetchBoardData() {
+            try {
+                let response = await fetch('http://127.0.0.1:8000/api/getboard/', {
+                    method: "GET",
+                });
+
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+
+                let data = await response.json();
+                console.log("LOG = ", data);
+
+                setData(data);
+            } catch (err) {
+                setError(err.message);
+            }
+        }
+
+        fetchBoardData();
+    }, []);
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    if (data.length === 0) {
+        return <div>Loading...</div>;
+    }
+
+    function AddMember() {
+        alert("No Functionality")
     }
 
     return (
@@ -20,6 +57,20 @@ function ShowGroup() {
             {
                 formShow && <GroupAddForm />
             }
+
+            {data ? (
+                data.boards
+                    .map((item, id) => (
+                        <div className={style.card} key={id}>
+                            <span onClick={AddMember} className={style.icon}>...</span>
+                            <h2>{item.name}</h2>
+                            <p>{item.description}</p>
+                            <p>Created by: {item.created_by}</p>
+                        </div>
+                    ))
+            ) : (
+                <p>No groups available.</p>
+            )}
 
         </div>
     )
